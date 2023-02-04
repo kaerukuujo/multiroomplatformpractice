eacff469653e9fbbef4b78616852ef3baea1afaa
kaerukuujo <mrjolyne@gmail.com>
2023-02-04 15:08:05 +0000

    collision blocks in game and rendered

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
+const parsedCollisions = collisionsLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D()
+
+const backgroundLevel1 = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: 'img/backgroundLevel1.png',    
+});
+
 const player = new Player();
 
 const keys = {
@@ -20,8 +31,11 @@ const keys = {
 
 function animate(){
     window.requestAnimationFrame(animate);
-    c.fillStyle = 'white'
-    c.fillRect(0,0, canvas.width, canvas.height);
+
+    backgroundLevel1.draw();
+    collisionBlocks.forEach((collisionBlock) =>{
+        collisionBlock.draw();
+    })
 
     player.velocity.x = 0;
     if(keys.d.pressed) player.velocity.x = 5;
